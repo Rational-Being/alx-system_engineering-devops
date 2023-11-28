fd3dfae63b6f2a78d8d187c58ebb42d46da84e04
Rational-Being <overcomer021@gmail.com>
2023-11-28 09:36:18 -0800

    100-textme.rb

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 #this script use regex to match the word school
 
-puts ARGV[0].scan(/\[from:(.*)\].*\[to:(.*)\].*\[flags:(.*?)\]/).join
+puts ARGV[0].scan(/\[from:(.*?)\].*to:(.*?)\].*\[flags:(.*?)\]/).join(",")
