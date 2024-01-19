6c0faa61db4fc4091b4a228473b879febe619316
Rational-Being <overcomer021@gmail.com>
2024-01-19 12:29:57 -0800

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,16 @@
 #manifest to install a program called flask
 
+package {'python3-pip':
+    ensure => installed,
+}
+
 package { 'flask':
     ensure   => '2.1.0',
     provider => 'pip3'
+}
+
+package { 'werkzeug':
+    ensure   => '2.0.1',
+    provider => 'pip',
+    require  => package['pyton3-pip'],
 }
