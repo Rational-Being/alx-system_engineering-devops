b23defb764031297f6c8cbbcbe581c403b7c4fef
Rational-Being <overcomer021@gmail.com>
2024-01-24 19:58:12 +0100

    7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -4,12 +4,16 @@ package { 'nginx:
 	ensure => installed,
 }
 
-file_line { '301 redirection':
-	ensure => 
-	path => '/etc/nginx/sites-available/default',
-	after => 'listen 80 default server;',
-	line => 'rewrite ^/redirect_me https://abdullateef.tech permanent;',
+exec { 'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https:\/\/abdullateef-yusuff.tec\/;\\n\\t}" /etc/nginx/sites-available/default':
+	provider => shell,
 }
+
+#file_line { '301 redirection':
+#	ensure => 
+#	path => '/etc/nginx/sites-available/default',
+#	after => 'listen 80 default server;',
+#	line => 'rewrite ^/redirect_me https://abdullateef.tech permanent;',
+#}
 
 file { '/var/www/html/index.html':
 	content => 'Hello World!',
