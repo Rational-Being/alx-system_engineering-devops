bb7e1d92a56602a3bfc1b3f449b5f96ba1b81c0e
Rational-Being <overcomer021@gmail.com>
2024-01-29 12:26:45 -0800

    2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,20 +1,20 @@
 #usind puppet to automate creating a cuat HTTP header response
 
 exec {'apt-update':
-	command => '/usr/bin/apt-get -y update'
+  command => '/usr/bin/apt-get -y update'
 }
 
 package { 'nginx':
-	ensure => 'installed',
-	name => 'nginx',
+  ensure => 'installed',
+  name   => 'nginx',
 }
 
 file_line { 'setting nginx header':
-	path => '/etc/nginx/nginx.conf',
-	line => "\tadd_header X-Served-By ${hostname};",
-	after => 'http {',
+  path  => '/etc/nginx/nginx.conf',
+  line  => "\tadd_header X-Served-By ${hostname};",
+  after => 'http {',
 }
 
 exec { 'run nginx':
-	command => '/usr/sbin/service nginx restart'
+  command => '/usr/sbin/service nginx restart'
 }
