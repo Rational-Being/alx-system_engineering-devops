85aecf52c0dc0ea0fa47a53928d567a22168bf99
root <overcomer021@gmail.com>
2024-02-19 11:29:16 -0800

    2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -12,20 +12,21 @@ from sys import argv
 
 if __name__ == "__main__":
     _id = argv[1]
-    base_url = "https://jsonplaceholder.typicode.com.users/{}".format(_id)
+    base_url = "https://jsonplaceholder.typicode.com/users/{}".format(_id)
     username = get(base_url).json().get('username')
    
-    task_url = "https://jsonplaceholder.typicode.com/todos"
+    task_url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(_id)
     task_response = get(task_url).json()
     
-    total_tasks = []
+    total_tasks = {_id: []}
+
     for task in task_response:
-        if (task.get("userId")):
-            dictionary = {}
-            dictionary["task"] = task.get("title")
-            dictionary["completed"] = task.get("completed")
-            dictionary["username"] = username
-            total_tasks.append(dictionary)
+        if (task.get("userId") == int(_id)):
+            total_tasks[_id].append({
+                "task": task.get("title"),
+                "completed": task.get("completed"),
+                "username": username
+                })
     
-    with open("{}.json".format(_id), "w") as file:
-        dump({_id: dictionary}, file)
+    with open("{}.json".format(_id), "w+") as file:
+        dump(total_tasks, file)
