4e1e278455cb1c7d35244f8dfb1c00a4e21d127c
Rational-Being <overcomer021@gmail.com>
2024-03-06 06:30:11 -0800

    0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,6 @@
 #this puppet code debugs a server error in a wordpress website
 exec { 'remove phpp':
+  command => "sed -i 's/class-wp-local.phpp/class-wp-locale.php/g' /var/www/ht\
+ml/wp-settings.php",
   path    => '/bin'
-  command => "sed -i 's/class-wp-local.phpp/class-wp-locale.php/g' /var/www/html/wp-settings.php",
 }
