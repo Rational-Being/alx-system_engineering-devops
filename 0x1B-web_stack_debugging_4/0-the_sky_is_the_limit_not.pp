9728e407a8d7ee477d6fa2982d4b19492bc077d8
Rational-Being <overcomer021@gmail.com>
2024-03-14 13:30:56 +0100

    0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,16 +1,11 @@
 #this pupet script will fix nginx request limit error
 exec { 'increase limit':
-  path    => '/bin/:/usr/bin/',
+  path    => '/bin:/usr/bin',
   command => 'sed -i "s/15/4096/g" /etc/default/nginx',
-  require => Package[nginx],
 }
 
 exec { 'restart nginx':
-  path    => '/usr/bin/',
+  path    => '/usr/bin',
   command => 'service nginx restart'
 }
 
-service { 'nginx':
-  ensure  => running,
-  enable  => true,
-}
